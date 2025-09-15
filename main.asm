cdcea5d4da9e9f90b35e200b4530f5abf498c9e0
Braulio Becerra <bbecerra14@student.rccd.edu>
2025-09-14 22:05:09 -0700

    Added code to print hello message

--- main.asm
+++ main.asm
@@ -3,10 +3,18 @@
 ; Begin data section
 section		.data
 
+SYS_WRITE			equ		1
 SYS_EXIT			equ		60
 
 EXIT_SUCCESS		equ		0
-;EXIT_SOMEFAIL		equ 	137
+EXIT_SOMEFAIL		equ 	137
+
+FD_STDIN			equ		0
+FD_STDOUT			equ		1
+FD_STDERR			equ		2
+
+HELLO_STRING		db		"Hello, my name is Braulio Becerra",13,10
+HELLO_STRING_LEN	equ		$-HELLO_STRING
 
 ; Begin uninitialized data section
 section		.bss
@@ -20,10 +28,17 @@ section		.text
 global _start
 _start:
 
-	; Setup the return value and call the exit syscall
-	mov rax, SYS_EXIT
-	;mov rdi, EXIT_SOMEFAIL
-	mov rdi, EXIT_SUCCESS
+	;	Print a Hello.
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, HELLO_STRING
+	mov rdx, HELLO_STRING_LEN
 	syscall
+	
+	; Setup the return value and call the exit syscall
+	mov rax, SYS_EXIT				; Move SYS_EXIT value into rax
+	mov rdi, EXIT_SUCCESS			; Move EXIT_SUCCESS value into rdi
+	;mov rdi, EXIT_SOMEFAIL
+	syscall							; Launch system call
 
 
