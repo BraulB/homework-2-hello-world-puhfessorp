8f188f2a27290f5de37ce97b01bf91679752ed5a
vboxuser <vboxuser@Box.myguest.virtualbox.org>
2025-09-14 21:16:46 -0700

    added main.asm file. Also fixed  Makefile issues

---
+++ main.asm
@@ -0,0 +1,22 @@
+
+
+; Begin data section
+section		.data
+
+SYS_EXIT			equ		60
+
+EXIT_SUCCESS		equ		0
+;EXIT_SOMEFAIL		equ 	137
+
+; Begin uninitialized data section
+section		.bss
+
+
+; Begin text section
+section		.text
+
+
+;	Entry point to our program
+
+
+
